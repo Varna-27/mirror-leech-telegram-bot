ec764faed4a3657a16f2386acb5290126391cfe3
Varna-27 <71973403+Varna-27@users.noreply.github.com>
2022-11-03 07:19:32 +0530

    CONFIG_FILE_URL

--- update.py
+++ update.py
@@ -13,6 +13,18 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
+CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL', '')
+if len(CONFIG_FILE_URL) != 0:
+    try:
+        res = rget(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open('config.env', 'wb+') as f:
+                f.write(res.content)
+        else:
+            log_error(f"Failed to download config.env {res.status_code}")
+    except Exception as e:
+        log_error(f"CONFIG_FILE_URL: {e}")
+
 load_dotenv('config.env', override=True)
 
 try:
